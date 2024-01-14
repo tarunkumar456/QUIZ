cdf3a4fd4fc5233299a2c60874f36d0f078b5a30
tarunkumar456 <122202430+tarunkumar456@users.noreply.github.com>
2024-01-14 14:33:57 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,7 +3,14 @@ const { getData, registerUser, loginUser, logout, addData, isAuth } = require('.
 const isaunthenticated = require('../middleware/auth');
 
 const router = express.Router();
+router.get('/', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
+    res.send('cors problem fixed:)');
+});
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
