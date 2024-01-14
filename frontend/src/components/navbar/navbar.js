abf460e5d7360b22151c07e59744d5e98d51fd9c
tarunkumar456 <tk8657270@gmail.com>
2024-01-14 23:53:32 +0530

    fav

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import { HStack, Image, Heading, Button } from '@chakra-ui/react'
 import { React, useState, useEffect } from 'react'
-// import img1 from '../assets/1.png'
+import img1 from '../../assets/1.png' 
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useAlert } from 'react-alert'
@@ -41,10 +41,10 @@ function Header() {
   })
   return (
     <HStack w={'100vw'} h={['10vh', '12vh']} pl={['3', '10']} bgColor={'blue.200'} alignItems={'center'} borderBottom={'1px solid blue'} position={'fixed'} zIndex={1000}>
-      {/* <Image src={img1} alt={'loading'} w={['10', '20']} h={['7', '20']} ></Image> */}
+      <Image src={img1} alt={'loading'} w={['8', '16']} h={['8', '16']} ></Image>
       <Heading fontSize={['3xl', '6xl']} color={'blue.500'} fontFamily={'sans-serif'}>Quiz-Time</Heading>
-      {login && <Button colorScheme='blue' mt={'6'} fontSize={['1.5vmax', '1vmax']} onClick={profilehandler} margin={'1vmax'} style={{ marginLeft: 'auto' }}>Profile</Button>}
-      {login && <Button colorScheme='blue' mt={'6'} fontSize={['1.5vmax', '1vmax']} onClick={logouthandler} margin={'1vmax'} >Logout</Button>}
+      {login && <Button colorScheme='blue' mt={'6'} fontSize={['1vmax', '1vmax']} onClick={profilehandler} margin={'0.5vmax'} style={{ marginLeft: 'auto' }}>Profile</Button>}
+      {login && <Button colorScheme='blue' mt={'6'} fontSize={['1vmax', '1vmax']} onClick={logouthandler} margin={'0.5vmax'} >Logout</Button>}
     </HStack>
   )
 }
