dd1eae1866c955aa140718195515c04f42713277
tarunkumar456 <tk8657270@gmail.com>
2024-01-16 16:30:33 +0530

    16th

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import { HStack, Image, Heading, Button } from '@chakra-ui/react'
 import { React, useState, useEffect } from 'react'
-import img1 from '../../assets/1.png' 
+import img1 from '../../assets/1.png'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useAlert } from 'react-alert'
@@ -46,9 +46,11 @@ function Header() {
     <HStack w={'100vw'} h={['10vh', '12vh']} pl={['3', '10']} bgColor={'blue.200'} alignItems={'center'} borderBottom={'1px solid blue'} position={'fixed'} zIndex={1000}>
       <Image src={img1} alt={'loading'} w={['8', '16']} h={['8', '16']} ></Image>
       <Heading fontSize={['2xl', '6xl']} color={'blue.500'} fontFamily={'sans-serif'}>Quiz-Time</Heading>
-      {login && <Button colorScheme='blue' mt={'6'} p={['1.5','3']} h={['7','10']} fontSize={['1vmax', '1vmax']} onClick={boardhandler} margin={['0.2vmax','0.5vmax']} style={{ marginLeft: 'auto' }}>Leaderboard</Button>}
-      {login && <Button colorScheme='blue' mt={'6'} p={['1.5','3']} h={['7','10']} fontSize={['1vmax', '1vmax']} onClick={profilehandler} margin={['0.2vmax','0.5vmax']} >Profile</Button>}
-      {login && <Button colorScheme='blue' mt={'6'} p={['1.5','3']} h={['7','10']} fontSize={['1vmax', '1vmax']} onClick={logouthandler} margin={['0.2vmax','0.5vmax']} >Logout</Button>}
+      <Box style={{ marginLeft: 'auto' }}>
+        {login && <Button colorScheme='blue' mt={'6'} p={['1.5', '3']} h={['7', '10']} fontSize={['1vmax', '1vmax']} onClick={boardhandler} margin={['0.2vmax', '0.5vmax']} >Leaderboard</Button>}
+        {login && <Button colorScheme='blue' mt={'6'} p={['1.5', '3']} h={['7', '10']} fontSize={['1vmax', '1vmax']} onClick={profilehandler} margin={['0.2vmax', '0.5vmax']} >Profile</Button>}
+        {login && <Button colorScheme='blue' mt={'6'} p={['1.5', '3']} h={['7', '10']} fontSize={['1vmax', '1vmax']} onClick={logouthandler} marginLeft={['0.2vmax', '0.5vmax']} marginY={['0.2vmax', '0.5vmax']} marginRight={'2vmax'}>Logout</Button>}
+      </Box>
     </HStack>
   )
 }
